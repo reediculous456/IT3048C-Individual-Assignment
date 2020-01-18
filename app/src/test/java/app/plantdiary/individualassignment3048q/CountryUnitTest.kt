bed91c7ba1f08d3312c1326ccf3f32afd364bd71
jonesbr <brandan.jones@uc.edu>
2020-01-18 14:20:48 -0500

    Initial commit

---
+++ app/src/test/java/app/plantdiary/individualassignment3048q/CountryUnitTest.kt
@@ -0,0 +1,32 @@
+package app.plantdiary.individualassignment3048q
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CountryUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun countryDTO_maintainsState() {
+
+    }
+
+    @Test
+    fun countryDTO_isPopulated() {
+
+    }
+
+    @Test
+    fun countryDTO_containsBelize() {
+
+    }
+}
