a4fa442695ea6d810b6fea36f1234328dbdad890
jonesbr <brandan.jones@uc.edu>
2020-01-18 14:38:40 -0500

    TDD Unit Tests

--- app/src/test/java/app/plantdiary/individualassignment3048q/CountryUnitTest.kt
+++ app/src/test/java/app/plantdiary/individualassignment3048q/CountryUnitTest.kt
@@ -1,8 +1,13 @@
 package app.plantdiary.individualassignment3048q
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import app.plantdiary.individualassignment3048q.ui.main.MainViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.TestRule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +15,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class CountryUnitTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    lateinit var mvm: MainViewModel
+
+    @Before
+    fun populateCountries() {
+        mvm = MainViewModel()
+
+    }
+
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
@@ -17,16 +34,43 @@ class CountryUnitTest {
 
     @Test
     fun countryDTO_maintainsState() {
-
+        var country = Country("NZ", "New Zealand")
+        assertTrue(country.code.equals("NZ") )
+        assertTrue(country.name.equals("New Zealand"))
     }
 
     @Test
     fun countryDTO_isPopulated() {
+        givenViewModelIsInitialized()
+        whenJSONDataAreReadAndParsed()
+        thenTheCollectionSizeShouldBeGreaterThanZero()
+    }
+
+    private fun givenViewModelIsInitialized() {
 
     }
 
+    private fun whenJSONDataAreReadAndParsed() {
+        mvm.fetchCountries()
+    }
+
+    private fun thenTheCollectionSizeShouldBeGreaterThanZero() {
+        mvm.countries.observeForever{
+            assertNotNull(it)
+            assertTrue(it.size > 0)
+        }
+    }
+
+
     @Test
     fun countryDTO_containsBelize() {
+        givenViewModelIsInitialized()
+        whenJSONDataAreReadAndParsed()
+        thenResultsShouldContainBelize()
+    }
+
+    private fun thenResultsShouldContainBelize() {
+        var containsBelize:Boolean = false
 
     }
 }
