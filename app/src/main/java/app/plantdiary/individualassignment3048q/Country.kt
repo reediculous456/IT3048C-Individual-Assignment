7c395f467e0af7ee06456ec3110d3280fdd399fe
Wesley Reed <reed.wesley.s@gmail.com>
2020-06-08 23:26:49 -0400

    project builds now

---
+++ app/src/main/java/app/plantdiary/individualassignment3048q/Country.kt
@@ -0,0 +1,4 @@
+package app.plantdiary.individualassignment3048q
+
+data class Country(val code: String, val name: String) {
+}
--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -1,7 +1,14 @@
 package app.plantdiary.individualassignment3048q.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.plantdiary.individualassignment3048q.Country
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
+
+    fun fetchCountries(): Unit {
+        TODO()
+    }
 }
