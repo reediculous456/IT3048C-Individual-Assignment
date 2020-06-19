40249170ac49806e5e3fe7ab45577271621a97d7
Wesley Reed <reed.wesley.s@gmail.com>
2020-06-18 22:59:29 -0400

    Maps (#4)

--- app/src/main/java/app/plantdiary/individualassignment3048q/RetrofitClientInstance.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/RetrofitClientInstance.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClientInstance {
-    private var retrofit: Retrofit? = null;
-    private val BASE_URL = "https://datahub.io/core/";
+    private var retrofit: Retrofit? = null
+    private val BASE_URL = "https://datahub.io/core/"
 
     val retrofitInstance: Retrofit?
         get() {
--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainFragment.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainFragment.kt
@@ -1,15 +1,26 @@
 package app.plantdiary.individualassignment3048q.ui.main
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import app.plantdiary.individualassignment3048q.R
+import app.plantdiary.individualassignment3048q.dto.Country
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.main_fragment.*
+
 
 class MainFragment : Fragment() {
 
@@ -18,21 +29,72 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var mMap: GoogleMap
+    private var mapReady = false
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        mMap = googleMap
+        mapReady = true
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.countries.observe(this, Observer{
-            countries -> actCountry.setAdapter(ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, countries))
+        viewModel.countries.observe(this, Observer { countries ->
+            actCountry.setAdapter(
+                ArrayAdapter(
+                    context!!,
+                    R.layout.support_simple_spinner_dropdown_item,
+                    countries
+                )
+            )
         })
         viewModel.fetchCountries()
+
+        actCountry.onItemClickListener = OnItemClickListener { parent, arg1, pos, id ->
+            val country = parent.getItemAtPosition(pos)
+            updateMap(country as Country)
+        }
+    }
+
+    fun getLocationFromAddress(context: Context?, strAddress: String?): LatLng? {
+        val coder = Geocoder(context)
+        val address: List<Address>?
+        var p1: LatLng? = null
+        try {
+            address = coder.getFromLocationName(strAddress, 5)
+            if (address == null) {
+                return null
+            }
+            val location: Address = address[0]
+            p1 = LatLng(location.latitude, location.longitude)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return p1
+    }
+
+    private fun updateMap(country: Country) {
+        if (mapReady && country != null) {
+            val location = getLocationFromAddress(context, country.name)
+            if (location != null) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude)))
+            }
+        }
     }
 
 }
